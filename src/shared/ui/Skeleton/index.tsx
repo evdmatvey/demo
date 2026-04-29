083629ea87ea1096ce0c4e08276446652076f9da
evdmatvey <evd.matvey@mail.ru>
2026-04-29 10:01:48 +0700

    Add Skeleton shared component

---
+++ src/shared/ui/Skeleton/index.tsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.css';
+
+interface SkeletonProps {
+  width?: string;
+  height?: string;
+}
+
+export const Skeleton = ({
+  width = '100%',
+  height = '20px',
+}: SkeletonProps) => {
+  return <div className={styles.root} style={{ width, height }}></div>;
+};
